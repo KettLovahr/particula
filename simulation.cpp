1a274d3acfec47f2e5e29adfff2ced6ce0fb05b7
Kett Lovahr <catusfelony@gmail.com>
2023-08-22 01:31:27 -0300

    Refactored the matrix initialization function

--- simulation.cpp
+++ simulation.cpp
@@ -1,92 +1,105 @@
 #include "simulation.h"
+#include <algorithm>
 #include <cstdint>
 #include <cstdio>
 
-
 Simulation::Simulation() {
-    width = 640;
-    height = 480;
-    initialize_matrix();
+  width = 640;
+  height = 480;
+  initialize_matrix();
 
-    matrix[120][120] = 1;
+  matrix[120][120] = 1;
 }
 
 void Simulation::initialize_matrix() {
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            matrix[i][j] = 0;
-            new_matrix[i][j] = 0;
-        }
-    }
+  std::fill(&matrix[0][0], &matrix[0][0] + width * height, 0);
+  std::fill(&new_matrix[0][0], &new_matrix[0][0] + width * height, 0);
 }
 
 void Simulation::execute_sim_step() {
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            if (get_from_coord(i, j) == 0) {continue;}
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      if (get_from_coord(i, j) == 0) {
+        continue;
+      }
 
-            if (!check_collision(i, j, types[get_from_coord(i, j)].move_to)) {
-                if (set_from_coord(i, j + 1, get_from_coord(i, j))) {
-                    set_from_coord(i, j, ParticleTypes::NONE);
-                } 
-            } else {
-                int choice = choose_from_fork(i, j, types[get_from_coord(i, j)].or_else);
-                if (set_from_coord(i + COL_POSITIONS[choice][0], j + COL_POSITIONS[choice][1], get_from_coord(i, j))) {
-                    set_from_coord(i, j, ParticleTypes::NONE);
-                }
-            }
+      if (!check_collision(i, j, types[get_from_coord(i, j)].move_to)) {
+        if (set_from_coord(i, j + 1, get_from_coord(i, j))) {
+          set_from_coord(i, j, ParticleTypes::NONE);
         }
+      } else {
+        int choice =
+            choose_from_fork(i, j, types[get_from_coord(i, j)].or_else);
+        if (set_from_coord(i + COL_POSITIONS[choice][0],
+                           j + COL_POSITIONS[choice][1],
+                           get_from_coord(i, j))) {
+          set_from_coord(i, j, ParticleTypes::NONE);
+        }
+      }
     }
-    iterations++;
-    copy_matrix();
+  }
+  iterations++;
+  copy_matrix();
 }
 
 void Simulation::copy_matrix() {
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            matrix[i][j] = new_matrix[i][j];
-        }
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      matrix[i][j] = new_matrix[i][j];
     }
+  }
 }
 
 int Simulation::get_from_coord(int x, int y) {
-    if (x > width || x < 0) {
-        return 255;
-    }
-    if (y > height || y < 0) {
-        return 255;
-    }
-    return matrix[x][y];
+  if (x > width || x < 0) {
+    return 255;
+  }
+  if (y > height || y < 0) {
+    return 255;
+  }
+  return matrix[x][y];
 }
 
 bool Simulation::set_from_coord(int x, int y, int value) {
-    if (x < 0) {return false;}
-    if (y < 0) {return false;}
-    if (x > width - 1) {return false;}
-    if (y > height - 1) {return false;}
+  if (x < 0) {
+    return false;
+  }
+  if (y < 0) {
+    return false;
+  }
+  if (x > width - 1) {
+    return false;
+  }
+  if (y > height - 1) {
+    return false;
+  }
+  if (new_matrix[x][y] != value) {
     new_matrix[x][y] = value;
     return true;
+  }
+  return false;
 }
 
-
 bool Simulation::check_collision(int x, int y, int bit) {
-    for (int i = 0; i < 8; i++) {
-        if ((bit >> (7-i) & 0x1) == 1) {
-            if (get_from_coord(x + COL_POSITIONS[i][0], y + COL_POSITIONS[i][1]) != 0) {
-                return true;
-            }
-        }
+  for (int i = 0; i < 8; i++) {
+    if ((bit >> (7 - i) & 0x1) == 1) {
+      if (get_from_coord(x + COL_POSITIONS[i][0], y + COL_POSITIONS[i][1]) !=
+          0) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 int Simulation::choose_from_fork(int x, int y, int move_to) {
-    for (int i = 0; i < 8; i++) {
-        if ((move_to >> (7-i) & 0x1) == 1) {
-            if (get_from_coord(x + COL_POSITIONS[i][0], y + COL_POSITIONS[i][1]) == 0) {
-                return i;
-            }
-        }
+  for (int i = 0; i < 8; i++) {
+    if ((move_to >> (7 - i) & 0x1) == 1) {
+      if (get_from_coord(x + COL_POSITIONS[i][0], y + COL_POSITIONS[i][1]) ==
+          0) {
+        return i;
+      }
     }
-    return -1;
+  }
+  return -1;
 }
